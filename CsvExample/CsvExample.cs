10f9ea40440d944d54883e48917ee26b14d1c130
tashinoki <kenya.112163@gmail.com>
2022-12-04 12:28:53 +0900

    read multiple records

---
+++ CsvExample/CsvExample.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogExample.CsvExample;
+
+internal abstract class CsvExample
+{
+    // Replace with the directory path where the csv is saved
+    protected const string DirectoryPath = @"";
+}
+
--- CsvExample/CsvReadExample.cs
+++ CsvExample/CsvReadExample.cs
@@ -3,14 +3,11 @@ using System.Globalization;
 
 namespace BlogExample.CsvExample;
 
-internal class CsvReadExample
+internal class CsvReadExample: CsvExample
 {
-    // Replace with the directory path where the csv is saved
-    private const string Path = @"";
-
     public void ReadCsv()
     {
-        using var reader = new StreamReader($@"{Path}\user.csv");
+        using var reader = new StreamReader($@"{DirectoryPath}\user.csv");
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         
         while(csv.Read())
@@ -18,6 +15,17 @@ internal class CsvReadExample
             var user = csv.GetRecord<UserRecord>();
             Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}");
         }
-        
+    }
+
+    public void ReadMultiRecords()
+    {
+        using var reader = new StreamReader($@"{DirectoryPath}\user.csv");
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        //var users = csv.GetRecords<UserRecord>().ToList();
+        var users = csv.GetRecords<UserRecord>();
+
+        foreach(var user in users)
+            Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Email: {user.Email}");
     }
 }
